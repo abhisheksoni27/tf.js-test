d6bf12c4157d9e82348f2b45813407a8707e34a1
Abhishek Soni <abhisheksoni2720@gmail.com>
2018-05-01 00:42:07 +0530

    tf.js

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import * as tf from '@tensorflow/tfjs';
-import {loadFrozenModel} from '@tensorflow/tfjs-converter'
 
-const canvas = document.getElementById('canvas');
+// Define a model for linear regression.
+const model = tf.sequential();
+model.add(tf.layers.dense({units: 1, inputShape: [1]}));
 
-const MODEL_URL = 'https://nofile.io/f/eGCEwUP1O3r/tensorflowjs_model.pb';
-const WEIGHTS_URL = 'https://nofile.io/f/y0ffA4k7tGU/weights_manifest.json';
+// Prepare the model for training: Specify the loss and the optimizer.
+model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
 
-const model = loadFrozenModel(MODEL_URL, WEIGHTS_URL,{credentials: 'include', mode:'no-cors'});
+// Generate some synthetic data for training.
+const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
+const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
+
+// Train the model using the data.
+model.fit(xs, ys).then(() => {
+  // Use the model to do inference on a data point the model hasn't seen before:
+  model.predict(tf.tensor2d([5], [1, 1])).print();
+});
